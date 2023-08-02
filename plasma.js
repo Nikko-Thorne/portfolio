d710f9920738b94f74791bc7eb32a372cd55b373
Nikko <xokkinx@gmail.com>
2023-08-02 15:42:02 -0700

    plasma mouse update

--- plasma.js
+++ plasma.js
@@ -10,7 +10,7 @@ let mouseX = null;
 let mouseY = null;
 
 const particles = [];
-const particleCount = 30;
+const particleCount = 100;
 
 // Create particles
 for (let i = 0; i < particleCount; i++) {
@@ -29,32 +29,36 @@ window.addEventListener("mousemove", (e) => {
 
 function drawLightning(x1, y1, x2, y2, segments) {
   ctx.beginPath();
-  ctx.moveTo(x1, y1);
-
-  let previousOffset = 0;
-
-  const gradient = ctx.createLinearGradient(x1, y1, x2, y2);
-  gradient.addColorStop(0, "rgba(255, 255, 255, 0.5)");
-  gradient.addColorStop(1, "rgba(0, 0, 139, 0.5)");
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+  let angle = Math.atan2(dy, dx);
+  let distance = Math.sqrt(dx * dx + dy * dy);
 
   for (let i = 0; i < segments; i++) {
     const t = i / segments;
-    const xt = x1 + (x2 - x1) * t;
-    const yt = y1 + (y2 - y1) * t;
-
-    const variance = Math.random() * 30 - 15 + previousOffset;
-    previousOffset = variance * -0.5;
-
-    ctx.lineTo(xt + variance, yt + variance);
+    const xt = x1 + dx * t;
+    const yt = y1 + dy * t;
+    const variance =
+      Math.sin(t * Math.PI * 4 + Date.now() * 0.002) *
+      (1 - t) *
+      (30 + Math.random() * 5);
+    const offsetX = Math.cos(angle - Math.PI / 2) * variance;
+    const offsetY = Math.sin(angle - Math.PI / 2) * variance;
+    ctx.lineTo(xt + offsetX, yt + offsetY);
   }
   ctx.lineTo(x2, y2);
-  ctx.strokeStyle = gradient;
-  ctx.lineWidth = 2;
+  ctx.strokeStyle = "rgba(255, 255, 255, 0.5)";
+  ctx.lineWidth = 3;
+  ctx.stroke();
+
+  // Additional drawing to give a 'glow' effect
+  ctx.strokeStyle = "rgba(173, 216, 230, 0.3)"; // Light blue color
+  ctx.lineWidth = 8;
   ctx.stroke();
 }
 
 function drawParticles() {
-  particles.forEach((particle) => {
+  particles.forEach((particle, index) => {
     if (mouseX && mouseY) {
       const dx = mouseX - particle.x;
       const dy = mouseY - particle.y;
@@ -62,6 +66,11 @@ function drawParticles() {
       const force = (1 / (distance * distance)) * 10000;
       particle.speedX += (dx / distance) * force;
       particle.speedY += (dy / distance) * force;
+    } else {
+      // Circular motion when not following the mouse
+      const angle = (Date.now() * 0.0005 + index) % (Math.PI * 2); // Continuous rotation
+      particle.speedX = Math.cos(angle) * 2; // Radius of circular motion
+      particle.speedY = Math.sin(angle) * 2; // Radius of circular motion
     }
 
     particle.x += particle.speedX;
@@ -78,10 +87,22 @@ function drawParticles() {
 
 function drawPlasmaOrb() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawParticles();
-  particles.forEach((targetParticle) => {
-    drawLightning(centerX, centerY, targetParticle.x, targetParticle.y, 10);
-  });
+
+  // Only draw if mouse coordinates are available
+  if (mouseX && mouseY) {
+    // Draw central plasma orb at mouse position
+    ctx.fillStyle = "rgba(173, 216, 230, 0.8)"; // Light blue color
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, 20, 0, Math.PI * 2);
+    ctx.fill();
+
+    drawParticles();
+    for (let i = 0; i < 5; i++) {
+      const targetParticle =
+        particles[Math.floor(Math.random() * particles.length)]; // Select random particles to shock
+      drawLightning(mouseX, mouseY, targetParticle.x, targetParticle.y, 10); // Using mouse coordinates
+    }
+  }
 }
 
 function animate() {
