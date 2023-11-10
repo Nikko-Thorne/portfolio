5eb44ef834ca8d6f181a439fb14342754082ebc8
xNikkox <xokkinx@gmail.com>
2023-11-09 19:06:00 -0800

    fixed css bug

--- javascript/nav.js
+++ javascript/nav.js
@@ -4,24 +4,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateActiveNavLink() {
     const sections = document.querySelectorAll("section, .hero#home");
-    const navbarHeight = document.querySelector("nav").offsetHeight;
-    let foundActive = false;
+    const viewportHeight = window.innerHeight;
+    let sectionFound = false;
+
+    // Set the "home" button to active by default
+    navLinks[0].classList.add("active");
 
     sections.forEach((section, index) => {
       const rect = section.getBoundingClientRect();
 
-      if (
-        !foundActive &&
-        rect.top <= window.innerHeight * 0.5 &&
-        rect.bottom >= window.innerHeight * 0.5
-      ) {
+      // Register the section as active when the bottom passes the bottom of the viewport
+      if (!sectionFound && rect.bottom >= viewportHeight && rect.top < viewportHeight) {
         navLinks.forEach((link) => link.classList.remove("active"));
         navLinks[index].classList.add("active");
-        foundActive = true;
+        sectionFound = true;
       }
     });
+
+    // If no sections are active, default to the "home" section
+    if (!sectionFound) {
+      navLinks.forEach((link) => link.classList.remove("active"));
+      navLinks[0].classList.add("active");
+    }
   }
 
   // Update active link on page load
   updateActiveNavLink();
+
+  // Add a scroll event listener to update the active link as the user scrolls
+  document.addEventListener('scroll', updateActiveNavLink);
 });
